efe04b41c59be582d9465cc3936df3f96a778432
arifvtti <arif.vtti@gmail.com>
2022-05-01 18:25:29 +0600

    emplement delete operation functionallity

--- src/pages/ManageItems/ManageItems.js
+++ src/pages/ManageItems/ManageItems.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import { Table } from 'react-bootstrap';
 import useItems from '../../hooks/useItems';
-import ManageItem from '../ManageItem/ManageItem';
+import "./ManageItems.css";
+import axios from "axios";
+import { toast, ToastContainer } from 'react-toastify';
 
 const ManageItems = () => {
     const [items, setItems] = useItems();
+
+    const handleDelete = async id => {
+        const { data } = await axios.post(`http://localhost:5000/item/${id}`);
+        setItems(items);
+        if (data.acknowledged) {
+            const rest = items.filter(item => item._id !== id);
+            setItems(rest);
+            toast("Delete operation success.")
+        }
+        console.log(data);
+    }
     return (
         <div className='container mx-auto d-block my-5'>
             <h3 className='fw-bold'>Manage All Product</h3>
             <hr />
-            <Table striped bordered hover size="sm">
+            <Table striped bordered hover size="sm" className="text-center">
                 <thead>
                     <tr>
                         <th>#</th>
@@ -19,16 +32,16 @@ const ManageItems = () => {
                 </thead>
                 <tbody>
                     {
-                        items.map(item => <tr key={item._id}>
+                        items.map(item => <tr key={item._id} >
                             <td></td>
                             <td>{item.name}</td>
-                            <td><button className='btn btn-danger'>Delete</button></td>
+                            <td><button onClick={() => handleDelete(item._id)} className='btn btn-danger'>Delete</button></td>
                         </tr>
                         )
                     }
                 </tbody>
             </Table>
-
+            <ToastContainer></ToastContainer>
         </div>
     );
 };
