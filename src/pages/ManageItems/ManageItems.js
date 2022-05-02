8ba82ecec9e80eab84babe434caccc9961b594b4
arifvtti <arif.vtti@gmail.com>
2022-05-02 16:10:19 +0600

    emplement modal for delete confirmation

--- src/pages/ManageItems/ManageItems.js
+++ src/pages/ManageItems/ManageItems.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Table } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Modal, Table } from 'react-bootstrap';
 import useItems from '../../hooks/useItems';
 import "./ManageItems.css";
 import axios from "axios";
@@ -8,16 +8,35 @@ import { toast, ToastContainer } from 'react-toastify';
 const ManageItems = () => {
     const [items, setItems] = useItems();
 
-    const handleDelete = async id => {
+    // Modal part
+    const [show, setShow] = useState(false);
+    const [agree, setAgree] = useState(false);
+    const [id, setId] = useState("");
+    const handleClose = () => setShow(false);
+    const handleSave = () => {
+        setAgree(true);
+        setShow(false);
+    }
+
+    const handleDelete = id => {
+        setShow(true);
+        setId(id);
+    }
+
+    const handlePermanentDelete = async id => {
+        setAgree(false);
         const { data } = await axios.post(`http://localhost:5000/item/${id}`);
-        setItems(items);
         if (data.acknowledged) {
             const rest = items.filter(item => item._id !== id);
             setItems(rest);
-            toast("Delete operation success.")
+            toast("Delete operation success.");
         }
-        console.log(data);
     }
+
+    if (agree) {
+        handlePermanentDelete(id);
+    }
+
     return (
         <div className='container mx-auto d-block my-5'>
             <h3 className='fw-bold'>Manage All Product</h3>
@@ -41,7 +60,22 @@ const ManageItems = () => {
                     }
                 </tbody>
             </Table>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title className='text-danger'>Warning for delete!!!</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Are you sure delete this data permanently?</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        No
+                    </Button>
+                    <Button variant="danger" onClick={handleSave}>
+                        Yes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
             <ToastContainer></ToastContainer>
+
         </div>
     );
 };
