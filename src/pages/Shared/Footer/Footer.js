668fc7f7f0f08830ca7ee17f643bda4194f2625c
arifvtti <arif.vtti@gmail.com>
2022-05-07 21:38:04 +0600

    readme file updated

--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <div className='bg-primary text-white p-2 rounded-3'>
+        <div className='bg-primary text-white p-2'>
 
             <div>
-                <div className='ps-5 container pt-5'>
-                    <h3 className='mb-5 fw-bold'>Laptop City</h3>
-                    <p><LocationMarkerIcon width={30} /> Level-4, 34, Awal Centre, Banani, Dhaka</p>
-                    <p><ChatAltIcon width={30} /> Official: web@laptop-city.com</p>
-                    <p><PhoneIcon width={30} /> Helpline : 01322810873 , 01322810867 , 01322810869
+                <div className='ps-md-5 container pt-md-5'>
+                    <h3 className='mb-md-5 fw-bold'>Laptop City</h3>
+                    <p><LocationMarkerIcon width={20} /> Level-4, 34, Awal Centre, Banani, Dhaka</p>
+                    <p><ChatAltIcon width={20} /> Official: web@laptop-city.com</p>
+                    <p><PhoneIcon width={20} /> Helpline : 01322810873 , 01322810867 , 01322810869
                         <br />
                         (Available : Sat - Thu, 10:00 AM to 7:00 PM)
                     </p>
